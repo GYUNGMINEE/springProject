db84251b722b14bd90a638eaff884fd9072f46ed
GYUNGMINEE <rudals03030@naver.com>
2023-10-18 10:58:54 +0900

    springProj

---
+++ src/main/java/kr/or/ddit/vo/Item3VO.java
@@ -0,0 +1,12 @@
+package kr.or.ddit.vo;
+
+import lombok.Data;
+
+//자바빈 클래스
+@Data
+public class Item3VO {
+	private int itemId;
+	private String itemName;
+	private int price;
+	private String description;
+}
